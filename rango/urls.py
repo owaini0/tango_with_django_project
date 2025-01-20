a5d161c277503600d6249fc40a8f7954337446a6
“Mansoor <2961033a@student.gla.ac.uk>
2025-01-20 20:19:34 +0000

    added templates

--- rango/urls.py
+++ rango/urls.py
@@ -1,8 +1,10 @@
 from django.urls import path
 from rango import views
+from rango import about
 
 app_name = 'rango'
 
 urlpatterns = [
-    path('', views.index, name='index')
+    path('', views.index, name='index'),
+    path('about/', views.about, name='about')
 ]
