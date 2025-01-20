d8d901595dd98f16fe03679bc9234817b350cefc
“Mansoor <2961033a@student.gla.ac.uk>
2025-01-20 20:19:34 +0000

    added templates

--- rango/views.py
+++ rango/views.py
@@ -2,4 +2,7 @@ from django.shortcuts import render
 from django.http import HttpResponse
 
 def index(request):
-    return HttpResponse("Hello World")
+    return HttpResponse('Hello World, Rango <a href="/rango/about">About Page</a> ')
+
+def about(request):
+    return HttpResponse('Rango says here is the about page, to go back to index <a href="/rango">click here</a>')
