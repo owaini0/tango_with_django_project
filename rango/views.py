85277497612bf9164fa81166002baf94037aa388
“Mansoor <2961033a@student.gla.ac.uk>
2025-02-01 17:13:41 +0000

    added categories view to the index

--- rango/views.py
+++ rango/views.py
@@ -1,8 +1,17 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from rango.models import Category
 
 def index(request):
-    return HttpResponse('Rango says hey there partner! <a href="/rango/about/">About</a> ')
+    
+    category_list = Category.objects.order_by('-likes')[:5]
+    
+    context_dict = {}
+    context_dict ['boldmessage'] = 'Crunchy, creamy, cookie, candy, cupcake!'
+    context_dict['categories'] = category_list
+    return render(request, 'rango/index.html', context_dict)
 
 def about(request):
-    return HttpResponse('Rango says here is the about page. <a href="/rango/">Index</a>')
+    tutorial_credit = {'name': 'Mansoor'}
+    return render(request, 'rango/about.html', tutorial_credit)
+
